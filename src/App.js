7796e94e4b428ecde75405607f17a0ce9fcccf5a
Afolabi Aiyeloja <obaone01@gmail.com>
2018-10-08 23:11:27 -0700

    Added CircleCI along with DotEnv and Size-Limit.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import logo from './logo.svg';
 import './App.css';
+
+const theme = createMuiTheme({});
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <Fragment className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React {this.props.name}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </Fragment>
+        <CssBaseline>
+        <MuiThemeProvider theme={theme}>
+          <Fragment className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React {this.props.name}</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </Fragment>
+        </MuiThemeProvider>
+        <CssBaseline/>
       </Router>
     );
   }
